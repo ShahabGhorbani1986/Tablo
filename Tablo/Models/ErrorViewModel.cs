2e5dfdb59557f01c3f0d2719de623260a75df0e2
shahab <74482197+ShahabGhorbani1986@users.noreply.github.com>
2023-10-06 12:59:31 +0330

    start

---
+++ Tablo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tablo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
